36079aec93c1a10e0e7a2ed06054715dedd440d5
kyungeun-j <jjaaaaaang22@gmail.com>
2021-07-31 05:16:28 +0900

    Update crawling.py

--- crawling.py
+++ crawling.py
@@ -1,7 +1,7 @@
 import requests
-import pprint
-
 from bs4 import BeautifulSoup 
+import json
+from collections import OrderedDict
 
 # HTTP GET Request
 req = requests.get('http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=11&ncvContSeq=&contSeq=&board_id=&gubun=').text
@@ -9,37 +9,33 @@ soup = BeautifulSoup(req, 'html.parser')
 
 selector = soup.select('#content > div > div.caseTable > div')
 
-arr = []
-for i in selector:
-    obj = {}
-    obj['ct'] = i.select_one('.ca_top').string
+title = ["covid_positive", "quarantine_release", "quarantine", "death"]
+sub_title = ["small_total", "from_korea", "from_hello"]
 
+TodayCount = OrderedDict()
+for i, t in zip(selector, title):
+    mainObj = OrderedDict()
     for j in i.select('ul > li:nth-child(1)'):
-        obj2 = {}
         if j.select_one('.ca_subtit') != None and j.select_one('.ca_value') != None:
-            obj2['cs'] = j.select_one('.ca_subtit').string
-            obj2['cv'] = j.select_one('.ca_value').string
-            obj['cc'] = obj2
+            mainObj['total'] = j.select_one('.ca_value').string
     
-    arr3 = []
     for j in i.select('ul > li:nth-child(2) > dl'):
-        # print(j)
-        obj3 = {}
-        if j.select_one('.ca_subtit') != None:
-            obj3['cs'] = j.select_one('.ca_subtit').string
         if j.select_one('.sum') != None:
-            arr4 = []
-            for k in j.select('dd > ul > li'):
-                obj4 = {}
-                obj4['it'] = k.select_one('.inner_tit').string
-                obj4['iv'] = k.select_one('.inner_value').string
-                arr4.append(obj4)
-                obj3['ii'] = arr4
+            subObj = OrderedDict()
+            for k, st in zip(j.select('dd > ul > li'), sub_title):
+                subObj[st] = k.select_one('.inner_value').string
+            mainObj['diff_yesterday'] = subObj
         if j.select_one('.txt_ntc') != None:
-            obj3['tn'] = j.select_one('.txt_ntc').string
-        arr3.append(obj3)
-        obj['ci'] = arr3
-        
-        # print(obj3)
-    arr.append(obj)
-pprint.pprint(arr)
+            mainObj['diff_yesterday'] = j.select_one('.txt_ntc').string
+    TodayCount[t] = mainObj
+print('TodayCount', json.dumps(TodayCount, ensure_ascii=False, indent="\t") )
+
+
+selector2 = soup.select('#content > div > div:nth-child(17) > table > tbody > tr')
+DailyCount = OrderedDict()
+for i,j in enumerate(selector2):
+    dayObj = OrderedDict()
+    dayObj['date'] = j.select_one('td:nth-child(1)').string
+    dayObj['total'] = j.select_one('td:nth-child(3)').string
+    DailyCount[i] = dayObj
+print('DailyCount', json.dumps(DailyCount, ensure_ascii=False, indent="\t") )
